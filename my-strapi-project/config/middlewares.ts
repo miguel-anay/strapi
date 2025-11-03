f2534e6ec19e5aadb4ca277235e70f07946744e5
miguel <mba.miguel18@gmail.com>
2025-11-02 20:56:38 -0500

    500

--- my-strapi-project/config/middlewares.ts
+++ my-strapi-project/config/middlewares.ts
@@ -1,7 +1,20 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
--- my-strapi-project/config/server.ts
+++ my-strapi-project/config/server.ts
@@ -4,4 +4,22 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  url: env('PUBLIC_URL', 'http://localhost:1337'),
+  proxy: true,
+  admin: {
+    url: '/admin',
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+      options: {
+        expiresIn: '7d',
+      },
+      sessions: {
+        cookieOptions: {
+          secure: env.bool('ADMIN_COOKIE_SECURE', false),
+          httpOnly: true,
+          sameSite: 'lax',
+        },
+      },
+    },
+  },
 });
